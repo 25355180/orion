ce658f7e956c6ff62ece72051d266457d94133df
25355180 <147062677+25355180@users.noreply.github.com>
2025-01-07 21:20:16 +0000

    2

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ class startScene extends Phaser.Scene {
             emitZone:{
                 type:'random',
                 source:new Phaser.Geom.Line(0,0,1800,600),
-                quantity:1,
+                quantity:2,
                 
             }
         };
