b952174020a10d7f8a9145c254866ff0092b2eb4
25355180 <147062677+25355180@users.noreply.github.com>
2025-01-08 12:12:59 +0000

    colours

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ class GamesDevCW2 extends Phaser.Scene{
             blendMode:'ADD', 
             frequency:12,
             setScale:2,
-            tint: [ 0xffff00, 0xffff0f, 0xfffff0, 0xfff0ff ],
+            tint: [ 0xfff000, 0xfff00f, 0xfff0f0, 0xfff0ff ],
 
             emitZone:{
                 type:'random',
